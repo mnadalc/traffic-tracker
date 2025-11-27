200e2ea27fdb6eb681988c2806a1737b31b3a08a
Miguel Nadal <mnadalc@gmail.com>
2025-11-27 11:31:18 +0100

    Initialitation of database

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -1,17 +1,27 @@
 package main
 
-import ("fmt"
+import (
+	"fmt"
 	"log"
+
 	"github.com/mnadalc/traffic-tracker/internal/config"
+	"github.com/mnadalc/traffic-tracker/internal/database"
 )
 
 func main() {
+	// Test
   fmt.Println("Hello, World!")
-	cfg, err := config.LoadConfig()
-	
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-	
+	cfg, _ := config.LoadConfig()
 	fmt.Println(cfg)
+
+	// Database
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
+	err = db.Init()
+	if err != nil {
+		log.Fatalf("Failed to init tables: %v", err)
+	}
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,3 +1,36 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "modernc.org/sqlite"
+)
+
 type DB struct {
-	conn *sql.DB  // sql.DB from "database/sql"
+	conn *sql.DB
+}
+
+func Connect() (*DB, error) {
+	conn, err := sql.Open("sqlite", "traffic.db")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{conn}, nil
+}
+
+// Execute routes and traffic data table creation
+func (db *DB) Init() error {
+	_, err := db.conn.Exec(createRoutesTable)
+	if err != nil {
+		return err
+	}
+	
+	_, err = db.conn.Exec(createTrafficDataTable)
+	if err != nil {
+		return err
+	}
+	
+	return nil
 }
